1343115cb6f992e43d9ca9da8920814804e9a324
Maneesh Madan <maneeshmadan@Abhishek-Banerjee-Mac.local>
2014-11-04 17:21:28 +0530

    MultipleScreens project completed from myside

--- CordovaProjects/WebProjects/MutipleScreens/index.js
+++ CordovaProjects/WebProjects/MutipleScreens/index.js
@@ -1,21 +1,47 @@
 
+document.getElementById("mySubmit").onclick = function(){ createDivision(); };
 
-function createDivision() {
+
+
+var inputColor;
+var inputText;
+
+
+function createDivision() {	
 	
+	 inputColor=  document.getElementById("inputColor").value;
+	 inputText=  document.getElementById("inputText").value;
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+	// Validation for empty textInput and colorInput textfields
+	if (inputText=="")
+	{
+		document.getElementById("inputText").style.borderColor="red"	;
+	}
 	
+	if (inputColor=="")
+	{
+		document.getElementById("inputColor").style.borderColor="red";
+	}
 	
-	
-	
+	else{
+		document.getElementById("inputText").style.borderColor="";
+		document.getElementById("inputColor").style.borderColor="";
+
+
 /*-------------------------------------------------------------------------------------------------------------------*/
 // Creating 1st Dynamic Divsion
 	var divElement1 = document.createElement("div");  
  	divElement1.id = "div1";
- 	divElement1.style.height="45%";
- 	divElement1.style.width="45%";
+ 	divElement1.style.height="30%";
+ 	divElement1.style.width="30%";
  	divElement1.style.background= "#FFFFFF";
  	divElement1.style.cssFloat= "left";
  	divElement1.style.border= "1px solid";
  	divElement1.style.borderTopLeftRadius = "2em";
+ 	divElement1.style.marginBottom="50px";
+ 	divElement1.style.marginRight="50px";
+ 	
 	document.body.appendChild(divElement1);
 
 // Creating 1st paragraph which will be appended to its parent div
@@ -33,12 +59,14 @@ function createDivision() {
 // Creating 2nd Dynamic Divsion
 	var divElement2 = document.createElement("div");  
  	divElement2.id = "div2"; 
- 	divElement2.style.height="45%";
- 	divElement2.style.width="45%";
+ 	divElement2.style.height="30%";
+ 	divElement2.style.width="30%";
  	divElement2.style.background= "#FFFFFF";
- 	divElement2.style.cssFloat= "left";
+ 	divElement2.style.cssFloat= "right";
  	divElement2.style.border= "1px solid";
  	divElement2.style.borderTopRightRadius = "2em"; 
+ 	divElement2.style.marginBottom="50px";
+ 	divElement2.style.marginLeft="50px";
 	document.body.appendChild(divElement2);
 
 // Creating 2nd paragraph which will be appended to its parent div
@@ -54,12 +82,14 @@ function createDivision() {
 // Creating 3rd Dynamic Divsion
 	var divElement3 = document.createElement("div");  
 	divElement3.id = "div3";
-	divElement3.style.height="45%";
- 	divElement3.style.width="45%";
+	divElement3.style.height="30%";
+ 	divElement3.style.width="30%";
 	divElement3.style.background= "#FFFFFF";
  	divElement3.style.cssFloat= "left";
  	divElement3.style.border= "1px solid";
 	divElement3.style.borderBottomLeftRadius = "2em";
+	divElement3.style.marginTop="50px";
+	divElement3.style.marginRight="50px";
 	document.body.appendChild(divElement3);
 
 // Creating 3rd paragraph which will be appended to its parent div
@@ -75,13 +105,15 @@ function createDivision() {
 // Creating 4th Dynamic Divsion
  	var divElement4 = document.createElement("div");  
  	divElement4.id = "div4";  
- 	divElement4.style.height="45%";
- 	divElement4.style.width="45%";
+ 	divElement4.style.height="30%";
+ 	divElement4.style.width="30%";
  	divElement4.style.background= "#FFFFFF";
- 	divElement4.style.cssFloat= "left";
+ 	divElement4.style.cssFloat= "right";
  	divElement4.style.border= "1px solid";
  	divElement4.style.borderBottomRightRadius = "2em";
- 	document.body.appendChild(divElement4);
+ 	divElement4.style.marginTop="50px";
+ 	divElement4.style.marginLeft="50px";
+  	document.body.appendChild(divElement4);
 
 // Creating 4th paragraph which will be appended to its parent div
 	var para4= document.createElement("p");
@@ -96,6 +128,8 @@ function createDivision() {
 /*-------------------------------------------------------------------------------------------------------------------*/	
 // Getting user inputs for text and color respectively
 	var inputColor= document.getElementById("inputColor").value;
+	var res= inputColor.split(",");
+	
 	var inputText= document.getElementById("inputText").value;
 /*-------------------------------------------------------------------------------------------------------------------*/
 
@@ -103,7 +137,7 @@ function createDivision() {
 /*-------------------------------------------------------------------------------------------------------------------*/	
 // Setting the text and its color in the 1st paragraph of the 1st division
 	document.getElementById("p1").innerHTML=inputText;
-	document.getElementById("p1").style.color=inputColor;
+	document.getElementById("p1").style.color=res[0];
 /*-------------------------------------------------------------------------------------------------------------------*/		
 
 
@@ -111,7 +145,7 @@ function createDivision() {
 /*-------------------------------------------------------------------------------------------------------------------*/	
 // Setting the text and its color in the 2nd paragraph of the 2nd division
 	document.getElementById("p2").innerHTML=inputText;
-	document.getElementById("p2").style.color=inputColor;
+	document.getElementById("p2").style.color=res[1];
 /*-------------------------------------------------------------------------------------------------------------------*/
 
 
@@ -119,7 +153,7 @@ function createDivision() {
 /*-------------------------------------------------------------------------------------------------------------------*/
 // Setting the text and its color in the 3rd paragraph of the 3rd division
 	document.getElementById("p3").innerHTML=inputText;
-	document.getElementById("p3").style.color=inputColor;
+	document.getElementById("p3").style.color=res[2];
 /*-------------------------------------------------------------------------------------------------------------------*/
 
 
@@ -127,8 +161,13 @@ function createDivision() {
 /*-------------------------------------------------------------------------------------------------------------------*/
 // Setting the text and its color in the 4th paragraph of the 4th division
 document.getElementById("p4").innerHTML=inputText;
-document.getElementById("p4").style.color=inputColor;
+if (typeof(res[3])!='undefined'){
+	document.getElementById("p4").style.color=res[3];
+	}
+
+else{
+	document.getElementById("p4").style.color="green";
+}
 /*-------------------------------------------------------------------------------------------------------------------*/
-
-
+}
 }
