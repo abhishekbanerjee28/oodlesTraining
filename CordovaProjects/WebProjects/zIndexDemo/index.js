a195c9074e7dd2f23c3a0529385f81bcba041671
Maneesh Madan <maneeshmadan@Abhishek-Banerjee-Mac.local>
2014-11-07 18:21:43 +0530

    new project created

---
+++ CordovaProjects/WebProjects/zIndexDemo/index.js
@@ -0,0 +1,39 @@
+/* window.onload=function(){
+      
+   
+
+	
+	var sp1= document.getElementById("span1");
+	var sp1Val1= sp1.innerHTML;
+	var sp2= document.getElementById("span2");
+	var sp2Val2= sp2.innerHTML;
+	var sp3= document.getElementById("span3");
+	var sp3Val3= sp3.innerHTML;
+	var sp4= document.getElementById("span4");
+	var sp4Val4= sp4.innerHTML;
+	//alert (sp1Val1 + "" + sp2Val2 + "" + sp3Val3 + "" + sp4Val4 );
+	sp1.style.backgroundColor= sp1Val1;
+	sp2.style.backgroundColor= sp2Val2;
+	sp3.style.backgroundColor= sp3Val3;
+	sp4.style.backgroundColor= sp4Val4;
+};	
+
+*/
+      
+   
+document.addEventListener("DOMContentLoaded", function(){
+	
+	var sp1= document.getElementById("span1");
+	var sp1Val1= sp1.innerHTML;
+	var sp2= document.getElementById("span2");
+	var sp2Val2= sp2.innerHTML;
+	var sp3= document.getElementById("span3");
+	var sp3Val3= sp3.innerHTML;
+	var sp4= document.getElementById("span4");
+	var sp4Val4= sp4.innerHTML;
+	//alert (sp1Val1 + "" + sp2Val2 + "" + sp3Val3 + "" + sp4Val4 );
+	sp1.style.backgroundColor= sp1Val1;
+	sp2.style.backgroundColor= sp2Val2;
+	sp3.style.backgroundColor= sp3Val3;
+	sp4.style.backgroundColor= sp4Val4;	
+});
