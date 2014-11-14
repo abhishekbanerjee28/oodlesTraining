8e2432887806e3441e7e2317253b9e29e8a85e88
Maneesh Madan <maneeshmadan@Abhishek-Banerjee-Mac.local>
2014-11-14 14:17:33 +0530

    weather app continued

--- CordovaProjects/WebProjects/weatherApp/index.js
+++ CordovaProjects/WebProjects/weatherApp/index.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+	
+	setInterval(function() {
+		var time = new Date();
+		time=time.toLocaleTimeString();
+		$('#localTime').text(time);
+	}, 1000);
+
 	var country, state, city;
+	$('#city').val("");
 	var weatherUrl = "http://api.wunderground.com/api/e1a1a0df617dddbc/conditions/q/";
-	var forecastURL= "http://api.wunderground.com/api/e1a1a0df617dddbc/forecast10day/q/";
+	var forecastURL = "http://api.wunderground.com/api/e1a1a0df617dddbc/forecast10day/q/";
 	$('#but').hide();
+	$('#gridView').hide();
+	$('#display').hide();
 
 	$('#submit').on('click', function() {
 		country = $('#country').val();
@@ -10,63 +20,86 @@ $(document).ready(function() {
 		city = $('#city').val();
 		if (city == "")
 			alert("enter city");
-		if (country == "USA") {
+		else {
 
-			ajaxCall(state, city, function(msg) {
+			if (country == "USA") {
 
-				if (msg.hasOwnProperty('current_observation')) {
-					$('#temp').text(msg ['current_observation']['temperature_string']);
+				ajaxCall(state, city, function(msg) {
 
-					//alert (msg ['current_observation']['local_time_rfc822'])
-					$('#localTime').text(msg ['current_observation']['local_time_rfc822']);
+					if (msg.hasOwnProperty('current_observation')) {
 
-					//alert ("weather " + msg ['current_observation']['weather']);
-					$('#weather').text(msg ['current_observation']['weather']);
+						$('#temp').text(msg ['current_observation']['temperature_string']);
 
-					//alert ("wind " + msg ['current_observation']['wind_string']);
-					$('#wind').text(msg ['current_observation']['wind_string']);
+						//alert (msg ['current_observation']['local_time_rfc822'])
+						$('#localTime').text(msg ['current_observation']['local_time_rfc822']);
 
-					//alert ("humidity " + msg ['current_observation']['relative_humidity']);
-					$('#humidity').text(msg ['current_observation']['relative_humidity']);
-					var img = msg ['current_observation']['icon_url'];
+						//alert ("weather " + msg ['current_observation']['weather']);
+						if ((msg ['current_observation']['weather'])==""){
+							alert(123456);
+							$('#weather').hide();
+						}
+						else{
+							
+						$('#weather').text(msg ['current_observation']['weather']);
+						}
 
-					$('<div><img src="' + img + '"/></div>').css('width', '100%').appendTo('#imageDiv');
+						//alert ("wind " + msg ['current_observation']['wind_string']);
+						$('#wind').text(msg ['current_observation']['wind_string']);
 
-					$('<div ui-input-btn ui-btn ui-corner-all" id="but"><input type="button" data-enhanced="true" value="Next 10 Days Forecast"/> </div>').appendTo('#but');
-					alert("button inserted");
-					$('#but').show();
-				} else
-					alert("Entered city is wrong");
+						//alert ("humidity " + msg ['current_observation']['relative_humidity']);
+						$('#humidity').text(msg ['current_observation']['relative_humidity']);
+						var img = msg ['current_observation']['icon_url'];
 
-			});
-		} else {
+						$('<div><img src="' + img + '"/></div>').css('width', '100%').appendTo('#imageDiv');
 
-			ajaxCall(country, city, function(msg) {
+						$('<div ui-input-btn ui-btn ui-corner-all" id="but"><input type="button" data-enhanced="true" value="Next 10 Days Forecast"/> </div>').appendTo('#but');
+						$('#display').show();
+						$('#but').show();
+					} else
+						alert("Entered city is wrong");
 
-				if (msg.hasOwnProperty('current_observation')) {
-					$('#temp').text(msg ['current_observation']['temperature_string']);
+				});
+			} else {
 
-					//alert (msg ['current_observation']['local_time_rfc822'])
-					$('#localTime').text(msg ['current_observation']['local_time_rfc822']);
+				ajaxCall(country, city, function(msg) {
 
-					//alert ("weather " + msg ['current_observation']['weather']);
-					$('#weather').text(msg ['current_observation']['weather']);
+					if (msg.hasOwnProperty('current_observation')) {
+						
+						$('#temp').text(msg ['current_observation']['temperature_string']);
 
-					//alert ("wind " + msg ['current_observation']['wind_string']);
-					$('#wind').text(msg ['current_observation']['wind_string']);
+						//alert (msg ['current_observation']['local_time_rfc822'])
+					//	$('#localTime').text(msg ['current_observation']['local_time_rfc822']);
+					 	
+						
+						//alert ("weather " + msg ['current_observation']['weather']);
+						if ((msg ['current_observation']['weather'])==""){
+							
+							$('#weather-div').hide();
+							
+						}
+						else{
+							
+							$('#weather').show();
+						$('#weather').text(msg ['current_observation']['weather']);
+						}
 
-					//alert ("humidity " + msg ['current_observation']['relative_humidity']);
-					$('#humidity').text(msg ['current_observation']['relative_humidity']);
+						//alert ("wind " + msg ['current_observation']['wind_string']);
+						$('#wind').text(msg ['current_observation']['wind_string']);
 
-					var img = msg ['current_observation']['icon_url'];
+						//alert ("humidity " + msg ['current_observation']['relative_humidity']);
+						$('#humidity').text(msg ['current_observation']['relative_humidity']);
 
-					$('<div><img src="' + img + '"/></div>').css('width', '100%').appendTo('#imageDiv');
-					$('#but').show();
+						var img = msg ['current_observation']['icon_url'];
 
-				} else
-					alert("Entered city is wrong");
+						$('<div><img src="' + img + '"/></div>').css('width', '100%').appendTo('#imageDiv');
+						$('#display').show();
+						$('#but').show();
 
-			});
+					} else
+						alert("Entered city is wrong");
+
+				});
+			}
 		}
 	});
 
@@ -106,20 +139,32 @@ $(document).ready(function() {
 		$('#humidity').text("");
 
 	});
-	
+
 	$('#but').on('click', function() {
-		
-			if (city == "")
+
+		if (city == "")
 			alert("enter city");
 		if (country == "USA") {
 
 			foreacast10days(state, city, function(msg) {
 
 				if (msg.hasOwnProperty('forecast')) {
-					for (var i=0;i<=19;i=i+2){
-						alert (i);
-						alert (msg.forecast.txt_forecast.forecastday[i].title);
-						}
+					for (var i = 0; i <= 9; i++) {
+
+						var date = msg.forecast.simpleforecast.forecastday[i].date.monthname + " " + msg.forecast.simpleforecast.forecastday[i].date.day + "," + msg.forecast.simpleforecast.forecastday[i].date.weekday;
+						var title = msg.forecast.simpleforecast.forecastday[i].date.weekday;
+						var img = msg.forecast.simpleforecast.forecastday[i].icon_url;
+						var humidity = msg.forecast.simpleforecast.forecastday[i].avehumidity;
+						var condition = msg.forecast.simpleforecast.forecastday[i].conditions;
+
+						var newdiv = $('<div> </div>').appendTo('#gridView');
+
+						$('<p> <h3>' + date + '</h3></p>').appendTo(newdiv);
+						$('<img src="' + img + '"/>').appendTo(newdiv);
+						$('<p> ' + condition + '</p>').appendTo(newdiv);
+						$('<p> ' + "<b>Avg Humidity: </b>" + humidity + "%" + '</p>').appendTo(newdiv);
+
+					}
 				} else
 					alert("Entered city is wrong");
 
@@ -127,12 +172,26 @@ $(document).ready(function() {
 		} else {
 
 			foreacast10days(country, city, function(msg) {
+				$('#gridView').show();
 
 				if (msg.hasOwnProperty('forecast')) {
-					for (var i=0;i<=19;i=i+2){
-						//alert (i);
-						alert (msg.forecast.txt_forecast.forecastday[i].title);
-						}
+					for (var i = 1; i <= 9; i++) {
+
+						var date = msg.forecast.simpleforecast.forecastday[i].date.monthname + " " + msg.forecast.simpleforecast.forecastday[i].date.day + "," + msg.forecast.simpleforecast.forecastday[i].date.weekday;
+						var title = msg.forecast.simpleforecast.forecastday[i].date.weekday;
+						var img = msg.forecast.simpleforecast.forecastday[i].icon_url;
+						var humidity = msg.forecast.simpleforecast.forecastday[i].avehumidity;
+						var condition = msg.forecast.simpleforecast.forecastday[i].conditions;
+
+						var newdiv = $('<div> </div>').appendTo('#gridView');
+
+						$('<p> <h3>' + date + '</h3></p>').appendTo(newdiv);
+						$('<img src="' + img + '"/>').appendTo(newdiv);
+						$('<p> ' + condition + '</p>').appendTo(newdiv);
+						$('<p> ' + "<b>Avg Humidity: </b>" + humidity + "%" + '</p>').appendTo(newdiv);
+
+					}
+
 				} else
 					alert("Entered city is wrong");
 
@@ -142,7 +201,7 @@ $(document).ready(function() {
 
 	function foreacast10days(con, ct, success) {
 		newURL = forecastURL + con + "/" + ct + ".json";
-		alert(newURL);
+	//	alert(newURL);
 		$.ajax({
 			url : newURL,
 			dataType : "jsonp",
